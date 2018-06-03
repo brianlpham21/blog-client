5b2d0be8a06aa2505a17cb354097e57196917da5
Brian Pham <brianlpham21@gmail.com>
2018-06-02 18:25:47 -0700

    set width to content 900px

--- src/components/main-carousel.js
+++ src/components/main-carousel.js
@@ -15,29 +15,31 @@ export class MainCarousel extends React.Component {
 
   render() {
     return (
-      <Carousel className="carousel">
-        <Carousel.Item>
-          <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div className="carousel-container">
+        <Carousel>
+          <Carousel.Item>
+            <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     );
   }
 }
---
+++ src/components/main-navbar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
+
+import {Navbar, NavItem, Nav} from 'react-bootstrap';
+
+export class MainNavbar extends React.Component {
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
+  render() {
+    // Only render the log out button if we are logged in
+    let logOutButton;
+    if (this.props.loggedIn) {
+      logOutButton = (
+        <button onClick={() => this.logOut()}>Log out</button>
+      );
+    }
+    return (
+      <div className="navbar-container">
+        <Navbar fixedTop collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Adventures</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Experiences
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Travel
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Tech
+              </NavItem>
+              <NavItem eventKey={4} href="/about">
+                About
+              </NavItem>
+            </Nav>
+            <Nav pullRight>
+             <NavItem eventKey={1} href="http://www.twitter.com" target="_blank">
+              <i className="fab fa-twitter fa-lg"></i>
+             </NavItem>
+             <NavItem eventKey={2} href="http://www.instagram.com" target="_blank">
+               <i className="fab fa-instagram fa-lg"></i>
+             </NavItem>
+             <NavItem eventKey={3} href="http://www.facebook.com" target="_blank">
+               <i className="fab fa-facebook fa-lg"></i>
+             </NavItem>
+             <NavItem eventKey={4} href="http://www.github.com" target="_blank">
+               <i className="fab fa-github fa-lg"></i>
+             </NavItem>
+           </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(MainNavbar);
