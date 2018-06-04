ae545e754752081ccddb582d65c27da0f06cc6ae
Brian Pham <brianlpham21@gmail.com>
2018-06-04 15:24:54 -0700

    styling footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,18 +9,39 @@ export function Footer(props) {
     <div className="footer-container bg-info">
       <Grid>
         <Row>
-          <Col md={6} mdPush={6}>
-            <p>Copyright &copy; 2018</p>
+          <Col md={4}>
+            <h4>Brians Adventures</h4>
           </Col>
-          <Col md={6} mdPull={6}>
+          <Col md={2}>
+            <h5>Explore</h5>
             <ul className="footer-links">
               <Link to="/about"><li>About</li></Link>
               <Link to="/category/experiences"><li>Experiences</li></Link>
               <Link to="/category/travel"><li>Travel</li></Link>
               <Link to="/category/tech"><li>Tech</li></Link>
+              <Link to="/contact"><li>Contact</li></Link>
+            </ul>
+          </Col>
+          <Col md={2}>
+            <h5>Follow</h5>
+            <ul className="footer-links">
+              <Link to="http://www.twttier.com"><li>Twitter</li></Link>
+              <Link to="http://www.instagram.com"><li>Instagram</li></Link>
+              <Link to="http://www.facebook.com"><li>Facebook</li></Link>
+              <Link to="http://www.github.com"><li>Github</li></Link>
+            </ul>
+          </Col>
+          <Col md={2}>
+            <h5>Admin</h5>
+            <ul className="footer-links">
               <Link to="/login"><li>Login</li></Link>
             </ul>
           </Col>
+        </Row>
+        <Row>
+        <Col className="footer-copyright">
+          <p>&copy; 2018 | Brians Adventures. All Rights Reserved.</p>
+        </Col>
         </Row>
       </Grid>
     </div>
--- src/components/main-article-section.js
+++ src/components/main-article-section.js
@@ -11,51 +11,59 @@ export function MainArticleSection(props) {
     <div className="main-article-container">
       <Grid>
         <Row className="article-row">
-          <Col md={6} mdPush={6}>
-            <Card className="card">
-              <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
-              <CardBody className="card-body">
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                <Button>Read More</Button>
-              </CardBody>
-            </Card>
+          <Col md={6}>
+            <Link to="#" className="card-link">
+              <Card className="card">
+                <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
+                <CardBody className="card-body">
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <Button>Read More</Button>
+                </CardBody>
+              </Card>
+            </Link>
           </Col>
-          <Col md={6} mdPull={6}>
-            <Card className="card">
-              <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
-              <CardBody className="card-body">
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                <Button>Read More</Button>
-              </CardBody>
-            </Card>
+          <Col md={6}>
+            <Link to="#" className="card-link">
+              <Card className="card">
+                <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
+                <CardBody className="card-body">
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <Button>Read More</Button>
+                </CardBody>
+              </Card>
+            </Link>
           </Col>
         </Row>
         <Row className="article-row">
-          <Col md={6} mdPush={6}>
-            <Card className="card">
-              <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
-              <CardBody className="card-body">
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                <Button>Read More</Button>
-              </CardBody>
-            </Card>
+          <Col md={6}>
+            <Link to="#" className="card-link">
+              <Card className="card">
+                <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
+                <CardBody className="card-body">
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <Button>Read More</Button>
+                </CardBody>
+              </Card>
+            </Link>
           </Col>
-          <Col md={6} mdPull={6}>
-            <Card className="card">
-              <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
-              <CardBody className="card-body">
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                <Button>Read More</Button>
-              </CardBody>
-            </Card>
+          <Col md={6}>
+            <Link to="#" className="card-link">
+              <Card className="card">
+                <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
+                <CardBody className="card-body">
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <Button>Read More</Button>
+                </CardBody>
+              </Card>
+            </Link>
           </Col>
         </Row>
       </Grid>
