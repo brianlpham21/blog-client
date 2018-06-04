7211a562d8cf3b0161766354bde9d2e43051ff8d
Brian Pham <brianlpham21@gmail.com>
2018-06-04 14:40:33 -0700

    added article card outline

--- src/components/main-article-section.js
+++ src/components/main-article-section.js
@@ -3,17 +3,59 @@ import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
 
 import {Grid, Row, Col} from 'react-bootstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button} from 'reactstrap';
+
 
 export function MainArticleSection(props) {
   return (
     <div className="main-article-container">
       <Grid>
-        <Row>
+        <Row className="article-row">
           <Col md={6} mdPush={6}>
-            Articles
+            <Card>
+              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
           </Col>
           <Col md={6} mdPull={6}>
-            Articles
+            <Card>
+              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="article-row">
+          <Col md={6} mdPush={6}>
+            <Card>
+              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md={6} mdPull={6}>
+            <Card>
+              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
           </Col>
         </Row>
       </Grid>
