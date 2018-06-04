f88ed933d6c2c00f0adb0fe0a594cfd36152fc43
Brian Pham <brianlpham21@gmail.com>
2018-06-03 23:24:09 -0700

    object fit carousel

--- src/components/main-carousel.js
+++ src/components/main-carousel.js
@@ -6,6 +6,7 @@ import {clearAuthToken} from '../local-storage';
 import {Carousel, Button} from 'react-bootstrap';
 
 import carousel from '../images/carousel.png';
+import camera from '../images/camera.jpg';
 
 export class MainCarousel extends React.Component {
   logOut() {
@@ -18,7 +19,7 @@ export class MainCarousel extends React.Component {
       <div className="carousel-container">
         <Carousel>
           <Carousel.Item>
-            <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
+            <a href="#"><img className="carousel-image" src={camera} /></a>
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -28,7 +29,7 @@ export class MainCarousel extends React.Component {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
+            <a href="#"><img className="carousel-image" src={camera} /></a>
             <Carousel.Caption>
               <h3>Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -38,7 +39,7 @@ export class MainCarousel extends React.Component {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <a href="#"><img width={900} height={500} alt="900x500" src={carousel} /></a>
+            <a href="#"><img className="carousel-image" src={camera} /></a>
             <Carousel.Caption>
               <h3>Third slide label</h3>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
