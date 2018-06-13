424ce3ce70d79e0d5887d49bd0b909578e8dd680
Brian Pham <brianlpham21@gmail.com>
2018-06-13 12:45:42 -0700

    retrieval for article section

--- src/components/main-article-section-2.js
+++ src/components/main-article-section-2.js
@@ -13,6 +13,12 @@ export class MainArticleSection2 extends React.Component {
   }
 
   render() {
+    if (this.props.posts.length < 1) {
+      return (
+        <div></div>
+      )
+    }
+
     return (
       <div className="main-article-container">
         <Grid>
@@ -22,9 +28,9 @@ export class MainArticleSection2 extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                    <CardTitle className="card-title">Article Title</CardTitle>
-                    <CardSubtitle className="card-date">Monday, June 4, 2018</CardSubtitle>
-                    <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                    <CardTitle className="card-title">{this.props.posts[7].title || 'N/A'}</CardTitle>
+                    <CardSubtitle className="card-date">{this.props.posts[7].date || 'N/A'}</CardSubtitle>
+                    <CardText className="card-text">{this.props.posts[7].titleSubtext || 'N/A'}</CardText>
                     <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
@@ -35,10 +41,10 @@ export class MainArticleSection2 extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1492970471430-bc6bd7eb2b13?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9893bc89e46e2b77a5d8c091fbba04e9&auto=format&fit=crop&w=2855&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                  <CardTitle className="card-title">Article Title</CardTitle>
-                  <CardSubtitle className="card-date">Date</CardSubtitle>
-                  <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                  <button className="article-button">Read More</button>
+                    <CardTitle className="card-title">{this.props.posts[8].title || 'N/A'}</CardTitle>
+                    <CardSubtitle className="card-date">{this.props.posts[8].date || 'N/A'}</CardSubtitle>
+                    <CardText className="card-text">{this.props.posts[8].titleSubtext || 'N/A'}</CardText>
+                    <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
               </Link>
@@ -50,10 +56,10 @@ export class MainArticleSection2 extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1506437942396-649fac10a75b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f369ac9dee5618d9719e6b7bed221387&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                  <CardTitle className="card-title">Article Title</CardTitle>
-                  <CardSubtitle className="card-date">Date</CardSubtitle>
-                  <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                  <button className="article-button">Read More</button>
+                    <CardTitle className="card-title">{this.props.posts[9].title || 'N/A'}</CardTitle>
+                    <CardSubtitle className="card-date">{this.props.posts[9].date || 'N/A'}</CardSubtitle>
+                    <CardText className="card-text">{this.props.posts[9].titleSubtext || 'N/A'}</CardText>
+                    <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
               </Link>
@@ -63,10 +69,10 @@ export class MainArticleSection2 extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1511702771955-42b52e1cd168?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0b8ce1412e0f8646fa9f794963ab897e&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                  <CardTitle className="card-title">Article Title</CardTitle>
-                  <CardSubtitle className="card-date">Date</CardSubtitle>
-                  <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                  <button className="article-button">Read More</button>
+                    <CardTitle className="card-title">{this.props.posts[10].title || 'N/A'}</CardTitle>
+                    <CardSubtitle className="card-date">{this.props.posts[10].date || 'N/A'}</CardSubtitle>
+                    <CardText className="card-text">{this.props.posts[10].titleSubtext || 'N/A'}</CardText>
+                    <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
               </Link>
--- src/components/main-article-section.js
+++ src/components/main-article-section.js
@@ -13,6 +13,12 @@ export class MainArticleSection extends React.Component {
   }
 
   render() {
+    if (this.props.posts.length < 1) {
+      return (
+        <div></div>
+      )
+    }
+
     return (
       <div className="main-article-container">
         <Grid>
@@ -22,9 +28,9 @@ export class MainArticleSection extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1494257473705-09a5a19d04af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8630a45d2003aa0fe3d2f5ff376d9ec&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                    <CardTitle className="card-title">Article Title</CardTitle>
-                    <CardSubtitle className="card-date">Monday, June 4, 2018</CardSubtitle>
-                    <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                    <CardTitle className="card-title">{this.props.posts[3].title || 'N/A'}</CardTitle>
+                    <CardSubtitle className="card-date">{this.props.posts[3].date || 'N/A'}</CardSubtitle>
+                    <CardText className="card-text">{this.props.posts[3].titleSubtext || 'N/A'}</CardText>
                     <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
@@ -35,9 +41,9 @@ export class MainArticleSection extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1492970471430-bc6bd7eb2b13?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9893bc89e46e2b77a5d8c091fbba04e9&auto=format&fit=crop&w=2855&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                  <CardTitle className="card-title">Article Title</CardTitle>
-                  <CardSubtitle className="card-date">Date</CardSubtitle>
-                  <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <CardTitle className="card-title">{this.props.posts[4].title || 'N/A'}</CardTitle>
+                  <CardSubtitle className="card-date">{this.props.posts[4].date || 'N/A'}</CardSubtitle>
+                  <CardText className="card-text">{this.props.posts[4].titleSubtext || 'N/A'}</CardText>
                   <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
@@ -50,9 +56,9 @@ export class MainArticleSection extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1506437942396-649fac10a75b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f369ac9dee5618d9719e6b7bed221387&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                  <CardTitle className="card-title">Article Title</CardTitle>
-                  <CardSubtitle className="card-date">Date</CardSubtitle>
-                  <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <CardTitle className="card-title">{this.props.posts[5].title || 'N/A'}</CardTitle>
+                  <CardSubtitle className="card-date">{this.props.posts[5].date || 'N/A'}</CardSubtitle>
+                  <CardText className="card-text">{this.props.posts[5].titleSubtext || 'N/A'}</CardText>
                   <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
@@ -63,9 +69,9 @@ export class MainArticleSection extends React.Component {
                 <Card className="card">
                   <CardImg className="card-image" top width="100%" src="https://images.unsplash.com/photo-1511702771955-42b52e1cd168?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0b8ce1412e0f8646fa9f794963ab897e&auto=format&fit=crop&w=1950&q=80" alt="Card image cap" />
                   <CardBody className="card-body">
-                  <CardTitle className="card-title">Article Title</CardTitle>
-                  <CardSubtitle className="card-date">Date</CardSubtitle>
-                  <CardText className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
+                  <CardTitle className="card-title">{this.props.posts[6].title || 'N/A'}</CardTitle>
+                  <CardSubtitle className="card-date">{this.props.posts[6].date || 'N/A'}</CardSubtitle>
+                  <CardText className="card-text">{this.props.posts[6].titleSubtext || 'N/A'}</CardText>
                   <button className="article-button">Read More</button>
                   </CardBody>
                 </Card>
