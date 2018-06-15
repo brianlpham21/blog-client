dab6a2b133ba955ae9b1ae30f16ed5fddb3fc253
Brian Pham <brianlpham21@gmail.com>
2018-06-15 09:28:20 -0700

    formatted text on page side

--- src/components/pages/travel-page.js
+++ src/components/pages/travel-page.js
@@ -53,7 +53,9 @@ export class TravelPage extends React.Component {
         <Grid>
           <Row>
             <Col md={4}>
-              Travel
+              <h3 className="page-title page-text">Travel</h3>
+              <p className="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.</p>
+              <p className="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.</p>
             </Col>
             <Col md={8} className="main-card">
               <Link to={link} className="card-link">
