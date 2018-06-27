85ef8b78bc26b35f9aa9ae0602a6d56e692cf852
Brian Pham <brianlpham21@gmail.com>
2018-06-26 20:20:51 -0700

    update load bar to landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -7,28 +7,43 @@ import MainArticleSection from './main-article-section';
 import MainArticleSection2 from './main-article-section-2';
 import LearnAboutMe from './learn-about-me';
 
-export function LandingPage(props) {
-  if (props.loggedIn) {
-    return <Redirect to="/dashboard" />;
+import {fetchPostsData} from '../actions/posts';
+
+export class LandingPage extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPostsData());
   }
 
-  return (
-    <div className="landing-page-container">
-      <MainCarousel />
-      <MainArticleSection />
-      <LearnAboutMe />
-      <MainArticleSection2 />
+  render() {
+    if (this.props.loggedIn) {
+      return <Redirect to="/dashboard" />;
+    }
 
-      <section id='instagram-feed'>
-        <h2 className='instagram-feed-title'>Instagram</h2>
-        <div id="instafeed" className='instafeed'></div>
-      </section>
-    </div>
-  );
+    if (this.props.posts.length < 1) {
+      return (
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      )
+    }
+
+    return (
+      <div className="landing-page-container">
+        <MainCarousel />
+        <MainArticleSection />
+        <LearnAboutMe />
+        <MainArticleSection2 />
+
+        <section id='instagram-feed'>
+          <h2 className='instagram-feed-title'>Instagram</h2>
+          <div id="instafeed" className='instafeed'></div>
+        </section>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-  loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null,
+  posts: state.postsData.posts
 });
 
 export default connect(mapStateToProps)(LandingPage);
