80caea530cef79beca202e2ebebe1d2418dda82f
Brian Pham <brianlpham21@gmail.com>
2018-07-11 11:48:42 -0700

    learn about me text

--- src/components/learn-about-me.js
+++ src/components/learn-about-me.js
@@ -7,7 +7,9 @@ export function LearnAboutMe(props) {
     <div className="learn-container" style={{backgroundImage: "url('https://images.unsplash.com/photo-1519865402023-40e4b7bfcba0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7ff1b07d24dfa8aead0b040f1681dbb4&auto=format&fit=crop&w=1950&q=80')"}}>
       <div className="learn-contents">
         <h2>Learn About Me</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.</p>
+        <p>The experiences that I have had throughout my life have definitely shaped
+        who I am as a person. I am always changing and growing and I hope that
+        logging my thoughts and experiences can help me map out who I want to be.</p>
         <Link to="/about"><button className="button">Learn More</button></Link>
       </div>
     </div>
