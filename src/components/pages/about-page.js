78738ea25f8e39c6868f8f35ac64d6d32126f19d
Brian Pham <brianlpham21@gmail.com>
2018-07-12 13:24:21 -0700

    edit about profile info

--- src/components/pages/about-page.js
+++ src/components/pages/about-page.js
@@ -19,10 +19,28 @@ export class AboutPage extends React.Component {
         <Grid>
           <Row className="about-main">
             <Col md={8}>
-              <h3>About Brian</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. </p>
-              <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. </p>
-              <p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. </p>
+              <h3>About Me</h3>
+              <p>My name is Brian Pham and I live in Orange County, California. If I
+              have to be specific, I live in Anaheim, California within walking distance
+              of Disneyland. I was born in Santa Clarita, California which is a small
+              town in the Greater Los Angeles Area where I grew up and nestled into the
+              safety of the suburban bubble there until I was 18.</p>
+              <p>After high school, I moved into a community in Orange County that might have
+              put me in more of a suburban bubble in Irvine, California. There I spent
+              4 years at the University of California, Irvine, where I was able to
+              grow and develop myself even further as a person.</p>
+              <p>Through many trials and tribulations, I have found myself here, existing
+              as a 28-year-old Asian-American male in Orange County, California. Recently, I
+              left my non-profit position to pursue a dream that I have had for the last
+              2 to 3 years. I had been studying web development and programming as well as
+              graphic design for some time before I realized that I would need to dedicate more
+              time to my studies on top of my full-time position. I have successfully
+              jumped that hurdle and am on the next chapter in my life.</p>
+              <p>Some of my interests and passions are also listed below for you to get
+              a better feel of my extracurriculars.</p>
+              <p>Note: I also love to travel so hopefully at some point, I will be able to
+              create a portion in my blog to dedicate to places that I have visted and places
+              where I would love to explore.</p>
             </Col>
             <Col md={4}>
               <Image className="about-image" src="https://lh3.googleusercontent.com/oxTlAQcf5yLD2wdGZaHTCAHVNgd0Rpfj1bgyNlHQv5chEQV3QyM31Q3jQMbpnKj1vkb1lCOn9AEhDh4-kZqiPhva8u4rHlM7suRf20mn9owehwMMCQcYLCunlHrD54whGF6HDtkmIUW8I7mhaB37YgYkp3w3A9mDZu_f9wk7O3brdeJq2gaq8l0OagoOtH7JFhiKqAMRVnWd_qI5-rHZsp2uTD_WcGs1c6cVYlixF-bPODn_imwD5MPRj-AHSalBmuXUJycdRMOIovtbK2gkdKd9o3HUqX1X_Uzyq3SgUJ9BHH-RASAHT99VhIbcfZHoTqaBhs6fkpIDfji2aJzhr4P0lLvHccOefC14gZ9xE-Yb6JZMSB459dLGYYiJA0Z8RCXHzyANk5h6MuEsnUJsYW_37l4DXiGD1PzwuB7EGQ7UQDsyJIQlYHq2sd2yeeJ9I2T2q2zHGcdxWIc_bJ4aNKk35DDaXF1sNLfP7CBq3WMn-fua3E7tDzqziJjLDFdiwWCmjN0ed4M9Ps8CCBi3TnjCvVrtGoqIsAR1ylwirc1SXHXaOGSXZLMk1sh2YWkPjuiQbyKeIEF6780-lYbDRTnEBbFV_p1RBcunslY=w750-h739-no" circle responsive />
